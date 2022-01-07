b2388333700855da2bf1ab2dafa8fd03836f4267
cgrocott1988 <cgrocott00@gmail.com>
2022-01-07 16:22:45 +0000

    Functionality complete

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const quote3 = 'When you give joy to other people, you get more joy in return. Y
 const quote4 = 'When you change your thoughts, remember to also change your world.';
 const quote5 = 'It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.';
 const quote6 = 'Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.';
-
+// All arrays in a batch variable
 const batch = [quote1, quote2, quote3, quote4, quote5, quote6];
+// Function to generate random number depending on array length.
+const randomQuote = (arr) => {
+  let random = Math.floor(Math.random() * arr.length);
+  return random;
+}
+// Run the program.
+const runProgram = () => console.log(`Heare is your quote: ${batch[randomQuote(batch)]}`);
+
+runProgram();
 
